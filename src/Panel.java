bfb34e21debd010efbb66e1438bf08a4673f7ef4
choco-mint1 <115313891+choco-mint1@users.noreply.github.com>
2023-07-09 14:18:23 -0500

    Setup initial structure

---
+++ src/Panel.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Panel extends JPanel implements ActionListener {
+
+    Panel() {
+
+    }
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
