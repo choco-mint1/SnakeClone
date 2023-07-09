d8e9a9752e1e7dfec6e979eaeffa708424a45942
choco-mint1 <115313891+choco-mint1@users.noreply.github.com>
2023-07-09 14:52:23 -0500

    Added game variables and constants

--- src/Panel.java
+++ src/Panel.java
@@ -1,9 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.util.Random;
 
 public class Panel extends JPanel implements ActionListener {
+
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
 
     Panel() {
 
@@ -38,4 +54,14 @@ public class Panel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
     }
+ public class MyKeyAdapter extends KeyAdapter{
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+ }
+
+
+
 }
