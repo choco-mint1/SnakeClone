4b0493bfb80bb70d19b150740dcb95fa9fdad4ac
choco-mint1 <115313891+choco-mint1@users.noreply.github.com>
2023-07-16 16:00:18 -0500

    Implemented apple eaten & snake growth mechanic

--- src/Panel.java
+++ src/Panel.java
@@ -90,7 +90,11 @@ public class Panel extends JPanel implements ActionListener {
 
 
     public void checkApple() {
-
+        if((x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
     }
 
     public void checkCollisions() {
